2942927f123230061c291b7d5f429859d31b40f3
safwaasamin <safwaasamin@gmail.com>
2024-12-05 12:55:34 +0530

    1

--- task3/fileWithCallbacks.js
+++ task3/fileWithCallbacks.js
@@ -3,26 +3,28 @@
 const fs = require('fs');
 
 // File to work with
-const filename = 'example.txt';
+// const filename = 'example.txt';
+
 
 // Write data to the file
-fs.writeFile(filename, 'Hello, this is a callback example!', (err) => {
-    if (err) {
-        console.error('Error writing to the file:', err);
-        return;
-    }
-    console.log('Data written successfully using callbacks.');
+// fs.writeFile(filename, 'Hello, this is a callback example!', (err) => {
+//     if (err) {
+//         console.error('Error writing to the file:', err);
+//         return;
+//     }
+//     console.log('Data written successfully using callbacks.');
 
-    // Read data from the file
-    fs.readFile(filename, 'utf8', (err, data) => {
-        if (err) {
-            console.error('Error reading the file:', err);
-            return;
-        }
-        console.log('Data read using callbacks:', data);
-    });
-});
+//     // Read data from the file
+//     fs.readFile(filename, 'utf8', (err, data) => {
+//         if (err) {
+//             console.error('Error reading the file:', err);
+//             return;
+//         }
+//         console.log('Data read using callbacks:', data);
+//     });
+// });
 
+fs.promises.writeFile("examplePromises.txt", "Hello, this is a promise example!")
 
 // For Output:
 // type in terminal
